b7f61e41918eff10ca3d0366d4aea792c213a1c9
laizuakram1 <laizuakram98@gmail.com>
2022-06-19 23:30:53 +0600

    create banner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import slider1 from '../../Images/Slider/slider1.jpg';
+import slider2 from '../../Images/Slider/slider2.jpg';
+import slider3 from '../../Images/Slider/slider3.jpg';
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is banner from homepage</h2>
-        </div>
+      <>
+        <div class="carousel w-full h-80">
+  <div id="item1" class="carousel-item w-full">
+    <img src={slider1} alt='yarn' class="w-full" />
+  </div> 
+  <div id="item2" class="carousel-item w-full">
+    <img src={slider2} alt='yarn' class="w-full" />
+  </div> 
+  <div id="item3" class="carousel-item w-full">
+    <img src={slider3} alt='yarn' class="w-full" />
+  </div> 
+ 
+</div> 
+<div class="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" class="btn btn-xs">1</a> 
+  <a href="#item2" class="btn btn-xs">2</a> 
+  <a href="#item3" class="btn btn-xs">3</a> 
+  
+</div>
+      </>
     );
 };
 
