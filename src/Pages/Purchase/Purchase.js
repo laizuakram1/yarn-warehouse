7d07f3431981f7b64e21128f08bf5bfd0e70430e
laizuakram1 <laizuakram98@gmail.com>
2022-06-22 01:21:46 +0600

    post purchase data to mongodb

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import Footer from '../Shared/Footer';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 
 const Purchase = () => {
     const { id } = useParams();
     const [product, setProduct] = useState([]);
+    const [user, loading, error] = useAuthState(auth);
+   
 
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = purchaseData => {
+        const url = (`http://localhost:5000/purchase`);
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(purchaseData)
+        })
+    }
+
+
+
 
     useEffect(() => {
         fetch(`http://localhost:5000/products/${id}`)
@@ -24,16 +41,16 @@ const Purchase = () => {
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-1'>
                 <div>
                     <form onSubmit={handleSubmit(onSubmit)} className='grid  gap-5 px-24 w-full'>
-                        <input type="text" {...register('name')} placeholder="Name"
+                        <input type="text" {...register('name')} placeholder={user?.displayName || 'Your Name'}
                             class="input input-bordered input-success w-full max-w-xs" />
 
-                        <input type="email" {...register('email')} placeholder="E-mail"
+                        <input type="email" {...register('email')} value={user?.email} placeholder={user?.email || 'E-mail'}
                             class="input input-bordered input-success w-full max-w-xs" />
 
-                        <input type="text" {...register('productName')} placeholder="Product Name"
+                        <input type="text" {...register('productName')} value={product?.name} placeholder={product?.name || 'Product Name'}
                             class="input input-bordered input-success w-full max-w-xs" />
 
-                        <input type="number" {...register('quantity')} placeholder="quantity"
+                        <input type="number" {...register('quantity')} placeholder={'quantity'}
                             class="input input-bordered input-success w-full max-w-xs" />
 
                         <input type="number" {...register('phone')} placeholder="Phone"
@@ -43,7 +60,7 @@ const Purchase = () => {
                             class="input input-bordered input-success w-full max-w-xs" />
 
                         <div className='justify-center'>
-                        <button type='submit' class="btn btn-success w-24">Purchase</button>
+                            <button type='submit' class="btn btn-success w-24">Purchase</button>
                         </div>
                     </form>
                 </div>
@@ -55,14 +72,14 @@ const Purchase = () => {
                             <p className='font-bold'>Price:${product.price}</p>
                             <p>available quantity: {product.quantity}/Role</p>
                             <p>Description:{product.description}</p>
-                           
+
                         </div>
                     </div>
                 </div>
             </div>
             <Footer></Footer>
         </div>
-        
+
     );
 };
 
