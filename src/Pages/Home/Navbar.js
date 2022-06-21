20edc2ed52be0d6ff38dd05d38578931387e08eb
laizuakram1 <laizuakram98@gmail.com>
2022-06-21 18:38:53 +0600

    handle signout on navbar

--- src/Pages/Home/Navbar.js
+++ src/Pages/Home/Navbar.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
+import Loading from '../Shared/Loading';
 
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    
+    const [user, loading, error] = useAuthState(auth);
+    console.log(user);
+    if (loading) {
+        return <Loading></Loading>
+    }
+    if (error) {
+        console.log(error.message);
+    }
+
+    const handleLogOut = () => {
+        signOut(auth);
+        navigate('/login')
+    };
+
     return (
         <div class="navbar bg-base-200">
             <div class="flex-1 w-100">
                 <Link className='text-3xl font-bold' to='/'>
-                   Laizu Yarn
+                    Laizu Yarn
                 </Link>
             </div>
             <ul class="menu menu-vertical lg:menu-horizontal bg-base-100 rounded-box mr-2">
@@ -15,7 +35,12 @@ const Navbar = () => {
                 <li><Link to='/dashboard'>Dashboard</Link></li>
                 <li><Link to='/blog'>Blog</Link></li>
                 <li><Link to='/about'>About</Link></li>
+                <li>{
+                    user? <button onClick={handleLogOut} class="btn btn-active btn-ghost">LogOut</button>
+                    : <Link to='/login'>Login</Link>
+                    }</li>
                 
+
             </ul>
             <div class="flex-none gap-2">
                 <div class="form-control">
@@ -24,17 +49,18 @@ const Navbar = () => {
                 <div class="dropdown dropdown-end">
                     <label tabindex="0" class="btn btn-ghost btn-circle avatar">
                         <div class="w-10 rounded-full">
-                            <img src="https://api.lorem.space/image/face?hash=33791" />
+                            <img src={user?.photoURL} alt='user' />
                         </div>
                     </label>
                     <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
                         <li>
-                            <a class="justify-between">
+                            <a className="justify-between">
                                 Profile
                                 <span class="badge">New</span>
                             </a>
                         </li>
-                        <li><Link to='/login'>Login</Link></li>
+                        <li><Link to='/'>Setting</Link></li>
+
                     </ul>
                 </div>
             </div>
---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SpinnerCircularSplit } from 'spinners-react';
+
+const Loading = () => {
+    return (
+        <div>
+            <SpinnerCircularSplit/>
+        </div>
+    );
+};
+
+export default Loading;
